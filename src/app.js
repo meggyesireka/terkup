47b17ae3b3413f7b14622da253a35720d7848848
Megyeri Márk Máté <megyko1234@gmail.com>
2023-01-31 19:48:20 +0100

    Fejrész hozzáadva

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+/*
+* File: app.java
+* Author: Meggyesi Réka
+* Copyright: 2023, Meggyesi Réka
+* Group: Szoft I/1/E
+* Date: 2023-01-31
+* Github: https://github.com/meggyesireka/
+* Licenc: GNU GPL
+*/
+
+
+
 const radiusElem = document.querySelector("#radius");
 const heightElem = document.querySelector('#height');
 const calcButton = document.querySelector('#calcButton');
